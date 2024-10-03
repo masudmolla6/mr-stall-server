376d9c266e801945ea4f062d793466a462937495
masudmolla6 <masudmolla2937@gmail.com>
2024-10-03 11:13:33 +0600

    added specific laptop api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,14 @@ async function run() {
             const result = await productCollections.find().toArray();
             res.send(result);
         })
+
+      app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollections.findOne(query);
+        console.log(result);
+        res.send(result);
+      })
 
 
     await client.connect();
